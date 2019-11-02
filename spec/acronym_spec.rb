abcb32a54e2e82feec4966a11c1197ea84accaaf
kordianceranowski <kordianceranowski@gmail.com>
2019-11-02 17:14:51 +0100

    mt2rspec

--- spec/acronym_spec.rb
+++ spec/acronym_spec.rb
@@ -1,4 +1,4 @@
-require '../lib/acronym'
+require 'acronym'
 
 RSpec.describe Acronym do
   describe '#abbreviate' do
@@ -8,7 +8,7 @@ RSpec.describe Acronym do
       expect(Acronym.abbreviate(string)).to eq out
     end
     it 'works with lowercase words' do
-      expect(Acronym.abbreviate('ROR')).to eq 'Ruby on Rails'
+      expect(Acronym.abbreviate("Ruby on Rails")).to eq "ROR"
     end
   end
 end
