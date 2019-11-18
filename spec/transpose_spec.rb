7e34ffd1646ef19e086e5f49fdaf1ab1115ffba2
kordianceranowski <kordianceranowski@gmail.com>
2019-11-18 13:01:03 +0100

    zmiana w spec_helper

--- spec/transpose_spec.rb
+++ spec/transpose_spec.rb
@@ -35,12 +35,6 @@ RSpec.describe Transpose do
     expected = "TT\nhh\nee\n  \nfs\nie\nrc\nso\ntn\n d\nl \nil\nni\nen\n.e\n ."
     expect(Transpose.transpose(input)).to(eq(expected))
   end
-  it("mixed line length") do
-    skip
-    input = "The longest line.\nA long line.\nA longer line.\nA line."
-    expected = "TAAA\nh   \nelll\n ooi\nlnnn\nogge\nn e.\nglr\nei \nsnl\ntei\n .n\nl e\ni .\nn\ne\n."
-    expect(Transpose.transpose(input)).to(eq(expected))
-  end
   it("square") do
     input = "HEART\nEMBER\nABUSE\nRESIN\nTREND"
     expected = "HEART\nEMBER\nABUSE\nRESIN\nTREND"
