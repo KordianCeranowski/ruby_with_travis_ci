3f19db213305a6288458b0b77bc9fb04a404c908
KordianCeranowski <kordianceranowski+github@gmail.com>
2019-12-08 14:07:43 +0100

    pierwsze testy z context

--- spec/two_fer_spec.rb
+++ spec/two_fer_spec.rb
@@ -1,13 +1,17 @@
 require 'two_fer' 
 
 RSpec.describe TwoFer do
-  it("no name given") do
-    expect(TwoFer.two_fer).to(eq("One for you, one for me."))
+  context 'when no value on input' do
+	it("no name given") do
+	  expect(TwoFer.two_fer).to(eq("One for you, one for me."))
+	end
   end
-  it("a name given") do
-    expect(TwoFer.two_fer("Alice")).to(eq("One for Alice, one for me."))
-  end
-  it("another name given") do
-    expect(TwoFer.two_fer("Bob")).to(eq("One for Bob, one for me."))
+  context 'when value is on input' do
+    it("a name given") do
+      expect(TwoFer.two_fer("Alice")).to(eq("One for Alice, one for me."))
+    end
+    it("another name given") do
+	  expect(TwoFer.two_fer("Bob")).to(eq("One for Bob, one for me."))
+    end
   end
 end
