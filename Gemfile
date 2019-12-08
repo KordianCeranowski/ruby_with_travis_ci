6b511d8c445dad397f2e1bbce4d83521dfe61b46
KordianCeranowski <kordianceranowski+github@gmail.com>
2019-12-08 13:58:42 +0100

    config changes

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source "https://rubygems.org"
 
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
 group :test do
   gem 'minitest', '~> 5.12.2'
   gem 'rspec', '~> 3.9.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
-# frozen_string_literal: true
-
-#require 'simplecov'
-#SimpleCov.start do
-#  add_filter '/spec/' # for rspec
-#  add_filter '/test/' # for minitest
-#end
-
 if ENV['TRAVIS'].to_s.empty?
   require 'simplecov'
   SimpleCov.coverage_dir("coverage/spec")
