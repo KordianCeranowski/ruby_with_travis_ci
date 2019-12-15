ad4522fa88d6464550f147e252e2fa7dac98524f
KordianCeranowski <KordianCeranowski@users.noreply.github.com>
2019-12-15 22:55:47 +0100

    adding contexts

--- spec/strings_integers_spec.rb
+++ spec/strings_integers_spec.rb
@@ -3,14 +3,15 @@
 require 'strings_integers'
 
 RSpec.describe String do
-  describe "#word_count" do
-    it "returns number of words in a string" do
-      string = "laughter, it's free"
-      expect(word_count(string)).to eq 3
-    end
-
-    it "returns 0 for empty string" do
-      expect(word_count("")).to eq 0
-    end
+  describe '#word_count' do
+		context 'when given a string'
+			it "returns number of words in a string" do
+				string = "laughter, it's free"
+				expect(word_count(string)).to eq 3
+			end
+			it "returns 0 for empty string" do
+				expect(word_count("")).to eq 0
+			end
+		end
   end
 end
--- spec/twelve_days_spec.rb
+++ spec/twelve_days_spec.rb
@@ -1,7 +1,9 @@
 require 'twelve_days' 
 
 RSpec.describe TwelveDays do
-  it("day 4") do
-    expect("On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.").to(eq(TwelveDays.sing_day(4)))
-  end
+	context 'when given a number returns part of a song'
+		it("day 4") do
+			expect("On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.").to(eq(TwelveDays.sing_day(4)))
+		end
+	end
 end
